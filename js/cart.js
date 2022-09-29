fe31361650345cedc7ce72abadd88afa6b694018
Jason Angus <jangus@umich.edu>
2022-09-29 20:45:56 +0000

    cart

--- js/cart.js
+++ js/cart.js
@@ -19,7 +19,14 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let tableRows = document.querySelectorAll('#cart tbody tr');
+  for(let i = 0; i < tableRows.length; i++){
+    if(tableRows[i]){
+      tableRows[i].remove();
+  }
+ }
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
@@ -27,10 +34,27 @@ function showCart() {
   // TODO: Find the table body
   let tableLocation = document.getElementById('cart').children[1];
   // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
-
+  for (let i = 0; i < Cart.length; i++) {
+    console.log(Cart[i]);
+    // TODO: Create a TR
+    let tableRow = document.createElement('tr');
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    let deleteLink = document.createElement('td');
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    let button = document.createElement('button');
+    button.textContent = 'X';
+    button.setAttribute('data-id', i);
+    button.addEventListener('click', removeItemFromCart);
+    deleteLink.appendChild(button);
+    tableRow.appendChild(deleteLink);
+    let quantityData = document.createElement('td');
+    quantityData.textContent = Cart[i].quantity;
+    tableRow.appendChild(quantityData);
+    let itemName = document.createElement('td');
+    itemName.textContent = Cart[i].item;
+    tableRow.appendChild(itemName);
+    tableLocation.appendChild(tableRow);
+  }
 }
 
 function removeItemFromCart(event) {
